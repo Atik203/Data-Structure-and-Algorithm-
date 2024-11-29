3335bdf326d67da2b742b528e554202b965a407a
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-11-29 21:11:08 +0600

    sorting problem 1

---
+++ Problem Solving/Sorting/1.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(vector<int> &a)
+{
+    int n = a.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[j] < a[min_index])
+            {
+                min_index = j;
+            }
+        }
+
+        swap(a[i], a[min_index]);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int k;
+    cin >> k;
+
+    selection_sort(a);
+
+    cout << a[k - 1] << endl;
+    return 0;
+}
---
+++ Problem Solving/Sorting/readme.md
@@ -0,0 +1,5 @@
+### Algorithms used for solving sorting problems:
+
+- Bubble Sort
+- Selection Sort
+- Insertion Sort
